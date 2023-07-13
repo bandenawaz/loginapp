79e77291436a3f456319a8de69c72134a00d08f1
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-13 15:09:04 +0530

    Final Code

--- app/src/main/java/com/sveri/loginapp/MainActivity.java
+++ app/src/main/java/com/sveri/loginapp/MainActivity.java
@@ -3,12 +3,55 @@ package com.sveri.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare
+    EditText etUser,etPwd;
+    Button btnSubmit, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialise
+        etUser = findViewById(R.id.editTextEmail);
+        etPwd = findViewById(R.id.editTextTextPassword);
+
+        btnSubmit = findViewById(R.id.buttonSubmit);
+        btnCancel = findViewById(R.id.buttonCancel);
+
+
+        //onclick handlers
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String user = etUser.getText().toString().trim();
+                String pwd = etPwd.getText().toString().trim();
+
+                if (user.equals("sveri") && pwd.equals("android")){
+                    Toast.makeText(MainActivity.this, "Authenticated",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                etUser.setText(null);
+                etPwd.setText(null);
+            }
+        });
     }
 }
